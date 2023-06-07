925aae6e242384d29a3c900f88c904df665736bb
DKonst21 <k.derienko@gmail.com>
2023-06-07 21:03:28 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from pathlib import Path
+
+
+Path("books").mkdir(parents=True, exist_ok=True)
+
+for number_books in range(1,11):
+    url = f'https://tululu.org/txt.php?id={number_books}'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    filename = f'books/id{number_books}.txt'
+    with open(filename, 'wb') as file:
+        print(file)
+        file.write(response.content)
