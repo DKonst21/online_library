77a75d385e196a1fd1c3b728b14f26128f8d8d46
DKonst21 <k.derienko@gmail.com>
2023-06-08 23:52:38 +0300

    download books with titles

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import requests
 from pathlib import Path
+from bs4 import BeautifulSoup
+from tululu import download_txt
 
 
 Path("books").mkdir(parents=True, exist_ok=True)
 
-for number_books in range(1,11):
-    url = f'https://tululu.org/txt.php?id={number_books}'
-    response = requests.get(url)
-    response.raise_for_status()
 
-    filename = f'books/id{number_books}.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+def check_for_redirect():
+    for number_books in range(1, 11):
+        url = f'https://tululu.org/b{number_books}/'
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        title_tag = soup.find('h1')
+        title_text = title_tag.text.split(':')
+        title_text_strip = title_text[0].strip()
+        filename = download_txt(url, title_text_strip)
+
+        try:
+            if response.history == []:
+                with open(filename, 'wb') as file:
+                    file.write(response.content)
+        except requests.exceptions.HTTPError():
+            raise Exception(response.url).with_traceback()
+
+    return response
+
+
+check_for_redirect()
---
+++ tululu.py
@@ -0,0 +1,17 @@
+import requests
+import os
+from pathvalidate import sanitize_filename
+
+
+def download_txt(url, filename, folder='books/'):
+    response = requests.get(url)
+    response.raise_for_status()
+    os.makedirs('{}'.format(folder), exist_ok=True)
+    name_split = filename.split('.')
+    name = sanitize_filename(f'{name_split[0]}.txt')
+    path = os.path.join(folder, name)
+
+    # with open(path, 'wb') as file:
+    #     file.write(response.content)
+    return path
+
